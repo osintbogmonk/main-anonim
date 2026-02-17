7cf80df302f51bbd8b74f56bbce26e3fb3b24630
osintbogmonk <lorenzorenaissance@gmail.com>
2026-02-17 17:54:58 +0300

    Update login.js

--- backend/frontend/frontend/login.js
+++ backend/frontend/frontend/login.js
@@ -1,11 +1,11 @@
 async function login() {
-  const email = document.getElementById("email").value;
+  const username = document.getElementById("username").value;
   const password = document.getElementById("password").value;
 
   const res = await fetch("https://main-anonim.onrender.com/auth/login", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ email, password })
+    body: JSON.stringify({ username, password })
   });
 
   const data = await res.json();
