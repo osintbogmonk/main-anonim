9c6012d9402704d5fe808952a9936820215f70c6
osintbogmonk <lorenzorenaissance@gmail.com>
2026-02-17 15:50:05 +0300

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,8 +1,9 @@
-import mongoose from "mongoose";
+const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
-  username: String,
-  password: String
-});
+  username: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+}, { timestamps: true });
 
-export default mongoose.model("User", UserSchema);
+module.exports = mongoose.model('User', UserSchema);
