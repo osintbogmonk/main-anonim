9f7a9b1bd1b152eec059111ef2357ef1236477a5
osintbogmonk <lorenzorenaissance@gmail.com>
2026-02-17 16:04:50 +0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,23 @@
 import express from "express";
 import cors from "cors";
 import "./db.js";
+
+// Routes
 import authRoutes from "./routes/auth.js";
+import userRoutes from "./routes/user.js";
+import chatRoutes from "./routes/chat.js";
+import messageRoutes from "./routes/message.js";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// Routes
+// Use routes
 app.use("/auth", authRoutes);
+app.use("/user", userRoutes);
+app.use("/chat", chatRoutes);
+app.use("/message", messageRoutes);
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
