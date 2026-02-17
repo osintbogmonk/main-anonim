33874729b43c4154773311d55037d36c21d35e36
osintbogmonk <lorenzorenaissance@gmail.com>
2026-02-17 19:41:49 +0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,16 @@ const express = require("express");
 const cors = require("cors");
 
 const authRoutes = require("./authRoutes");
+const chatRoutes = require("./chatRoutes"); // ← добавили чаты
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
+// Маршруты авторизации
 app.use("/auth", authRoutes);
+
+// Маршруты чатов
+app.use("/chat", chatRoutes); // ← добавили чаты
 
 app.listen(3000, () => console.log("Server running on port 3000"));
