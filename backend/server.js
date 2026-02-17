a2732281bc235ffc969de17bf7320934a62c4bde
osintbogmonk <lorenzorenaissance@gmail.com>
2026-02-17 15:52:29 +0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -4,9 +4,11 @@ import "./db.js";
 import authRoutes from "./routes/auth.js";
 
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 
+// Routes
 app.use("/auth", authRoutes);
 
 const PORT = process.env.PORT || 3001;
