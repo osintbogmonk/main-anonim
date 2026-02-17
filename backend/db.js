5206048d149fff03c02a0c10858280af6ab383b1
osintbogmonk <lorenzorenaissance@gmail.com>
2026-02-17 13:07:13 +0300

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,11 +1,8 @@
 import mongoose from "mongoose";
 
-export async function connectDB() {
-  try {
-    await mongoose.connect(process.env.MONGO_URL);
-    console.log("MongoDB connected");
-  } catch (err) {
-    console.error("DB error:", err);
-    process.exit(1);
-  }
-}
+mongoose.connect(process.env.MONGO_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => console.log("MongoDB connected"))
+.catch(err => console.error("MongoDB error:", err));
