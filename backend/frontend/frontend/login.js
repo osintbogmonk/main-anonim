9382c1d6b182172835af1d043fa8bac4d697ed5e
osintbogmonk <lorenzorenaissance@gmail.com>
2026-02-17 17:57:47 +0300

    Update login.js

--- backend/frontend/frontend/login.js
+++ backend/frontend/frontend/login.js
@@ -1,20 +1,21 @@
-async function login() {
+async function registerUser() {
   const username = document.getElementById("username").value;
+  const email = document.getElementById("email").value;
   const password = document.getElementById("password").value;
 
-  const res = await fetch("https://main-anonim.onrender.com/auth/login", {
+  const res = await fetch("https://main-anonim.onrender.com/auth/register", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ username, password })
+    body: JSON.stringify({ username, email, password })
   });
 
   const data = await res.json();
 
   if (!res.ok) {
-    alert(data.message || "Ошибка входа");
+    alert(data.message || "Ошибка регистрации");
     return;
   }
 
-  localStorage.setItem("token", data.token);
-  window.location.href = "chats.html";
+  alert("Успешно! Теперь войдите.");
+  window.location.href = "index.html";
 }
