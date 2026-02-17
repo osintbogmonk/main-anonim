a181d1516b9ebff0f5026298a343909190ed5573
osintbogmonk <lorenzorenaissance@gmail.com>
2026-02-17 16:01:54 +0300

    Create Message.js

---
+++ backend/models/backend/models/Message.js
@@ -0,0 +1,9 @@
+import mongoose from "mongoose";
+
+const MessageSchema = new mongoose.Schema({
+  chatId: { type: mongoose.Schema.Types.ObjectId, ref: "Chat" },
+  senderId: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+  text: { type: String, required: true }
+}, { timestamps: true });
+
+export default mongoose.model("Message", MessageSchema);
